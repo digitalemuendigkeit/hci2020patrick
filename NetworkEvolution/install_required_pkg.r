e6b5f812cffb6a090b13cc914e1fc2328042f9f7
patrick93h <patrick93h@gmail.com>
2020-02-11 15:24:54 +0100

    Updated Results CSV (Supernode Opinion Difference), switched to newer Rmd Template Version

--- NetworkEvolution/install_required_pkg.r
+++
@@ -1,7 +0,0 @@
-# This script will install the required packages
-
-install.packages("knitr")
-install.packages("tidyverse")
-install.packages("DT")
-install.packages("rmdformats")
-install.packages("prettydoc")
--- simulation/test/runtests.jl
+++ simulation/test/runtests.jl
@@ -18,6 +18,7 @@ results = DataFrame(
     EigenCentralityMean = Float64[],
     ClustCoeff = Float64[],
     CommunityCount = Int64[],
+    CommunityCountNontrivial = Int64[],
     ConnectedComponents = Int64[],
     OpinionSD = Float64[],
     OpChangeDeltaMean = Float64[],
@@ -27,6 +28,7 @@ results = DataFrame(
     SupernodeBetweenness = Float64[],
     SupernodeEigen = Float64[],
     SupernodeOpinion = Float64[],
+    Supernode1st2ndOpdiff = Float64[],
     CommunityOpMeanSDs = Float64[]
 )
 
@@ -38,7 +40,7 @@ for file in readdir("results")
         raw = load(joinpath("results", file))
         current_run = raw[first(keys(raw))]
         repcount = length(current_run)
-        supernode = [findmax(outdegree(current_run[i].final_state[1]))[2] for i in 1:repcount]
+        nodes_ranked = [first.(sort([(i, outdegree(current_run[j].final_state[1], i)) for i in 1:nv(current_run[j].final_state[1])], by=last, rev=true)) for j in 1:repcount]
 
         opinionsd = [std([agent.opinion for agent in current_run[i].final_state[2]]) for i in 1:repcount]
         opchange_delta_mean = [mean([abs(current_run[j].init_state[2][i].opinion - current_run[j].final_state[2][i].opinion) for i in 1:current_run[j].config.network.agent_count]) for j in 1:repcount]
@@ -51,19 +53,23 @@ for file in readdir("results")
         closeness_centrality_mean = [mean(closeness_centrality(current_run[i].final_state[1])) for i in 1:repcount]
         betweenness_centrality_mean = [mean(betweenness_centrality(current_run[i].final_state[1])) for i in 1:repcount]
         eigen_centrality_mean = [mean(eigenvector_centrality(current_run[i].final_state[1])) for i in 1:repcount]
-        supernode_outdegree = [outdegree(current_run[i].final_state[1], supernode[i]) for i in 1:repcount]
-        supernode_closeness = [closeness_centrality(current_run[i].final_state[1])[supernode[i]] for i in 1:repcount]
-        supernode_betweenness = [betweenness_centrality(current_run[i].final_state[1])[supernode[i]] for i in 1:repcount]
-        supernode_eigen = [eigenvector_centrality(current_run[i].final_state[1])[supernode[i]] for i in 1:repcount]
-        supernode_opinion = [current_run[i].final_state[2][supernode[i]].opinion for i in 1:repcount]
+        supernode_outdegree = [outdegree(current_run[i].final_state[1], nodes_ranked[i][1]) for i in 1:repcount]
+        supernode_closeness = [closeness_centrality(current_run[i].final_state[1])[nodes_ranked[i][1]] for i in 1:repcount]
+        supernode_betweenness = [betweenness_centrality(current_run[i].final_state[1])[nodes_ranked[i][1]] for i in 1:repcount]
+        supernode_eigen = [eigenvector_centrality(current_run[i].final_state[1])[nodes_ranked[i][1]] for i in 1:repcount]
+        supernode_opinion = [current_run[i].final_state[2][nodes_ranked[i][1]].opinion for i in 1:repcount]
+        supernode1st2nd_opdiff = [abs(current_run[i].final_state[2][nodes_ranked[i][1]].opinion - current_run[i].final_state[2][nodes_ranked[i][2]].opinion) for i in 1:repcount]
         clust_coeff = [global_clustering_coefficient(current_run[i].final_state[1]) for i in 1:repcount]
         conn_components = [length(connected_components(current_run[i].final_state[1])) for i in 1:repcount]
 
         # Calc the SD of the opinion means of the identified clusters
         n_communities = Int64[]
         community_opinion_mean_sds = Float64[]
+        n_communities_nontrivial = Int64[]
         for i in 1:repcount
             label_prop = label_propagation(current_run[i].final_state[1])[1]
+            community_sizes = last.(collect(countmap(label_prop)))
+            push!(n_communities_nontrivial, length([i for i in community_sizes if i > 1]))
             push!(n_communities, maximum(label_prop))
             if maximum(label_prop) == 1
                 push!(community_opinion_mean_sds, 0)
@@ -88,6 +94,7 @@ for file in readdir("results")
                 EigenCentralityMean = eigen_centrality_mean,
                 ClustCoeff = clust_coeff,
                 CommunityCount = n_communities,
+                CommunityCountNontrivial = n_communities_nontrivial,
                 ConnectedComponents = conn_components,
                 OpinionSD = opinionsd,
                 OpChangeDeltaMean = opchange_delta_mean,
@@ -97,6 +104,7 @@ for file in readdir("results")
                 SupernodeBetweenness = supernode_betweenness,
                 SupernodeEigen = supernode_eigen,
                 SupernodeOpinion = supernode_opinion,
+                Supernode1st2ndOpdiff = supernode1st2nd_opdiff,
                 CommunityOpMeanSDs = community_opinion_mean_sds
             )
         )
